09fab9001dbe2388d11e3587aa1ac29747218738
shayanj07 <39861006+shayanj07@users.noreply.github.com>
2018-07-12 12:13:56 -0700

    Update TCP Sniffer.py
    Incoming TCP-SYN/RST packet sniffer. Needs to be tested and modified for usage on windows and linux. The device that scapy sniffs on need to be determined automatically using netifaces. This feature will be added.

--- Shayan/TCP Sniffer.py
+++ Shayan/TCP Sniffer.py
@@ -1,13 +1,75 @@
 # Shayan Javid
 # Dr. Bartlett
-# TCP-SYN Sniffer
+# TCP-SYN Sniffer with Info
 from scapy.all import *
+import netifaces
+import sqlite3
+import os
+import platform
+import _datetime
+
+#creating a database
+sqlite_file = '~/Desktop/data.db'
+conn = sqlite3.connect (sqlite_file)
+c = conn.cursor()
+#creating table
+c.execute('CREATE TABLE GrayStar (packet_number INT, src_IP TEXT, src_port INT, dst_ip TEXT, dst_port INT, time TEXT, flag TEXT, len INT)')
+
+if platform.system() == 'Darwin' or platform.system() == 'Linux':
+    print("Hey you are in macOS/Linux")
+    os.system('lsof -i | grep LISTEN')
+elif platform.system() == 'Windows':
+    os.system('netstat -apn tcp')    # cmd does not have grep
+else:
+    print("Not able to detect the system.")
+
+netifaces.interfaces()
+
+netifaces.ifaddresses('en0')        #en0 = WiFi         en1 = Thunderbolt1      en2 = Thunderbolt2
+
+addrs = []
+
+for interface in netifaces.interfaces():
+
+    try:
+
+        print(netifaces.ifaddresses(interface)[netifaces.AF_INET])
+
+        for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+
+            addrs.append(link['addr'])
+
+    except Exception as e:
+
+        pass
+
+
+local_ip = addrs[1]
 
 cnt = 0
 def packet_format(packet):
+    currentDT = datetime.now()
     global cnt
-    cnt+=1
-    return "packet #{}: {} --> {}".format(cnt, packet[0][1].src, packet[0][1].dst)
-    
-    
-sniff (filter = "tcp[tcpflags] & 2 == 2 and tcp[tcpflags] & 16 == 0", prn=packet_format)
+    if local_ip == packet[0][1].dst:
+        cnt += 1
+        flag = packet.sprintf("%TCP.flags%")
+        if flag == 'R':
+            flag = 'RST'
+        elif flag == 'S':
+            flag = 'SYN'
+        else:
+            flag = packet.sprintf("%TCP.flags%")
+        length = len (packet)
+        c.execute ("INSERT INTO GrayStar2 VALUES (?,?,?,?,?,?,?,?)", (cnt, packet[0][1].src,packet.sport, packet[0][1].dst, packet.dport, str(currentDT.hour) + str(currentDT.minute) +str(currentDT.second), flag, length))
+        conn.commit()
+        return "packet #{}: Source IP: {}     Source Port: {}     --> Destination IP: {}      Destination Port: {}" \
+               "  *Time: {}:{}:{}   Flag: {} Length: {}"\
+            .format(cnt, packet[0][1].src,packet.sport, packet[0][1].dst, packet.dport, currentDT.hour,currentDT.minute
+                    ,currentDT.second, flag, length)
+
+
+sniff (filter = "tcp[tcpflags] & (tcp-ack) == 0 and (tcp-syn) != 0", prn=packet_format)
+conn.close()
+
+
+
